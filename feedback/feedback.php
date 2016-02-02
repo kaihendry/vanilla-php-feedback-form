a643a724f1163b56a6ef03f07c28bdfb936d0c84
Kai Hendry <hendry@webconverger.com>
2016-02-02 09:46:40 +0800

    Accept messages from Natalian

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,13 +1,18 @@
 <?php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-	// TODO: compare $_SERVER['HTTP_ORIGIN'] to the list of domains you'd like to allow
-	header("Access-Control-Allow-Origin: http://dabase.com");
-	// header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+switch ($_SERVER['HTTP_ORIGIN']) {
+	case "http://dabase.com":
+	case "http://feedback.dabase.com":
+	case "http://natalian":
+	case "https://natalian.org":
+		header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+		break;
+	default:
+		die("Bad origin: " . $_SERVER['HTTP_ORIGIN']);
 }
+
 
 // Access-Control headers are received during OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
 		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
@@ -22,12 +27,12 @@ $input = json_decode(file_get_contents('php://input'), true);
 
 if(empty($input["msg"])) { http_response_code(400); die(); }
 
-$message = $input["msg"] . "\n\n--\n" . print_r($_SERVER, true);
+$message = serialize($input["msg"]) . "\n\n--\n" . print_r($_SERVER, true);
 
 if(empty($input["from"]) || !filter_var($input["from"], FILTER_VALIDATE_EMAIL)) {
-	mail('root', $subject, $message);
+	mail('hendry+feedback@iki.fi', $subject, $message);
 } else {
 	$headers = 'From: ' . $input["from"];
-	mail('root', $subject, $message, $headers);
+	mail('hendry+feedback@iki.fi', $subject, $message, $headers);
 }
 ?>
