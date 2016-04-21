c6897da9b66cc20066ffd9aa7cbd234c7b253de6
Kai Hendry <hendry@iki.fi>
2016-04-21 04:21:49 +0000

    Some fancy styling

--- feedback/feedback.js
+++ feedback/feedback.js
@@ -8,13 +8,18 @@ function feedback(feedbackform) {
 		}
 	}
 
-	console.log(data);
+	feedbackform.send.value = "Sending...";
+	feedbackform.send.disabled = true;
 
 	fetch(feedbackform.action, { method: "POST", body: JSON.stringify(data) }).then(function(res){
 		if (res.ok) {
 			console.log(res);
+			feedbackform.send.value = "Sent!";
+			feedbackform.send.className = "success";
 		} else {
 			console.log("error", res);
+			feedbackform.send.value = "Error, try again";
+			feedbackform.send.disabled = false;
 		}
 	});
 
